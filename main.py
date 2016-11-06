234f118564d6097bf772e99d5622d5c193746c2d
Ioma Taani <iomataani@users.noreply.github.com>
2016-11-06 14:24:18 +0100

    sistemato flask

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 # importa flask
-from flask import Flask, request, send_from_directory, render_template
+from flask import *
 app = Flask(__name__, static_folder='static', static_url_path='')
 
 @app.route("/")
 def index():
-    return render_template('layout.html')
+    return render_template('index.html')
 
