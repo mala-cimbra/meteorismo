5978c8de5298f85f4892a6fa029047dc36282140
Ioma Taani <iomataani@users.noreply.github.com>
2016-11-06 13:15:53 +0100

    FINALMENTE sistemata la cartella static

--- main.py
+++ main.py
@@ -2,17 +2,7 @@
 
 # importa flask
 from flask import Flask, request, send_from_directory, render_template
-app = Flask(__name__, static_url_path='/static')
-
-# invio i js
-@app.route('/js/<path:path>')
-def send_js(path):
-    return send_from_directory('js', path)
-
-# invio i css
-@app.route('/css/<path:path>')
-def send_css(path):
-    return send_from_directory('css', path)
+app = Flask(__name__, static_folder='static', static_url_path='')
 
 @app.route("/")
 def index():
