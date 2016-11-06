8a1f1bef48defb1df684e49efa7930ac9da2391a
Ioma Taani <iomataani@users.noreply.github.com>
2016-11-06 12:17:19 +0100

    avanti

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
 # importa flask
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, send_from_directory, render_template
+app = Flask(__name__, static_url_path='/static')
+
+# invio i js
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+# invio i css
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path)
 
 @app.route("/")
 def index():
-    return "questa è l'index"
+    return render_template('layout.html')
 
-if __name__ == "__main__":
-    app.run()
